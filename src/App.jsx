f1a88e3cd51cce020f1a6abf896a91563fe91308
Antonio Rice <ricex407@gmail.com>
2024-05-28 16:08:02 -0500

    main layout

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [{ index: true, element: <Dashboard /> }],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
