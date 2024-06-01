3a7110b4eeef8c9aaa004218bd62d10e42b7e263
Antonio Rice <ricex407@gmail.com>
2024-05-31 20:25:31 -0500

    settings menu

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,24 +1,90 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
 
 const Settings = () => {
-  return (
-    <div>
-      <div className="flex pb-4">Settings</div>
+  const { user } = useContext(AuthContext);
+  console.log(user);
 
-      <dl className="max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
-        <div className="flex flex-col pb-3">
-          <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">User Name</dt>
-          <dd className="text-lg font-semibold">miguel_vhh</dd>
+  return (
+    <div className="p-10 ">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10 border-b-[.5px] border-gray-700">
+        <div className="md:col-span-1">
+          <h1>General</h1>
+          <p className="text-xs text-gray-600">Control basic general settings</p>
         </div>
-        <div className="flex flex-col py-3">
-          <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">Email Address</dt>
-          <dd className="text-lg font-semibold">miguel_vhh@vhh.com</dd>
+        <div className="md:col-span-2 flex flex-col gap-6">
+          <div className="flex flex-col md:flex-row gap-3 w-full">
+            <div className="w-full md:w-1/2 mb-6 items-center">
+              <label className="block uppercase tracking-wide text-[10px] font-light mb-2">First Name</label>
+              <input
+                className="appearance-none block w-full bg-[#111827] border border-gray-400 rounded-lg py-3 px-4 mb-3 leading-tight text-sm focus:outline-none focus:bg-[#141d2f]"
+                id="firstName"
+                type="text"
+                placeholder={user.first_name}
+              />
+            </div>
+            <div className="w-full md:w-1/2 mb-6">
+              <label className="block uppercase tracking-wide text-[10px] font-light mb-2">Last Name</label>
+              <input
+                className="appearance-none block w-full bg-[#111827] border border-gray-400 rounded-lg py-3 px-4 mb-3 leading-tight text-sm focus:outline-none focus:bg-[#141d2f]"
+                id="lastName"
+                type="text"
+                placeholder={user.last_name}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row gap-3 w-full">
+            <div className="w-full md:w-1/2 mb-6 items-center">
+              <label className="block uppercase tracking-wide text-[10px] font-light mb-2">Email</label>
+              <input
+                className="appearance-none block w-full bg-[#111827] border border-gray-400 rounded-lg py-3 px-4 mb-3 leading-tight text-sm focus:outline-none focus:bg-[#141d2f]"
+                id="email"
+                type="text"
+                placeholder={user.email}
+              />
+            </div>
+            <div className="w-full md:w-1/2 mb-6">
+              <label className="block uppercase tracking-wide text-[10px] font-light mb-2">Phone Number</label>
+              <input
+                className="appearance-none block w-full bg-[#111827] border border-gray-400 rounded-lg py-3 px-4 mb-3 leading-tight text-sm focus:outline-none focus:bg-[#141d2f]"
+                id="phoneNumber"
+                type="text"
+                placeholder={user.phone_number}
+              />
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col pt-3">
-          <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">Phone number</dt>
-          <dd className="text-lg font-semibold">+00 123 456 789 / +12 345 678</dd>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10 border-b-[.5px] border-gray-700">
+        <div className="md:col-span-1">
+          <h1>Preferences</h1>
+          <p className="text-xs text-gray-600">Control basic preference settings</p>
         </div>
-      </dl>
+        <div className="md:col-span-2 flex flex-col">
+          <div className="flex flex-col md:flex-row gap-10">
+            <div className="mb-6 items-center">
+              <label className="block uppercase tracking-wide text-[10px] font-light mb-2">Language</label>
+              <label className="inline-flex items-center cursor-pointer">
+                <input type="checkbox" value="" className="sr-only peer" />
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">EN / ES</span>
+              </label>
+            </div>
+            <div className="mb-6 items-center">
+              <label className="block uppercase tracking-wide text-[10px] font-light mb-2">Display</label>
+              <label className="inline-flex items-center cursor-pointer">
+                <input type="checkbox" value="" className="sr-only peer" />
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Light / Dark</span>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-end mt-6">
+        <button className="bg-slate-600 px-4 py-2 text-white rounded-lg">Save</button>
+      </div>
     </div>
   );
 };
