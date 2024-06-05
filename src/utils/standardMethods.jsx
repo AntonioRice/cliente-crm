89c208cb5e444534cb4c1d10eed5acf99af5a400
Antonio Rice <ricex407@gmail.com>
2024-06-05 17:23:22 -0500

    formatDatetime update

--- src/utils/standardMethods.jsx
+++ src/utils/standardMethods.jsx
@@ -1,5 +1,8 @@
 import moment from "moment";
 
 export const formatDateTime = (isoDate) => {
-  return isoDate !== null ? moment(isoDate).format("MM-DD-YYYY") : isoDate;
+  if (!isoDate || !moment(isoDate, moment.ISO_8601, true).isValid()) {
+    return null;
+  }
+  return moment(isoDate).format("MM-DD-YYYY");
 };
