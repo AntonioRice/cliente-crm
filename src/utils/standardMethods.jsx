39145facba0af1f8bec26880b54c5d80b4e897a1
Antonio Rice <ricex407@gmail.com>
2024-06-07 18:37:40 -0500

    lastVisit & total method

--- src/utils/standardMethods.jsx
+++ src/utils/standardMethods.jsx
@@ -6,3 +6,18 @@ export const formatDateTime = (isoDate) => {
   }
   return moment(isoDate).format("MM-DD-YYYY");
 };
+
+export const getLastVisit = (guest) => {
+  if (!guest.reservations || guest.reservations.length === 0) {
+    return null;
+  }
+
+  const lastReservation = guest.reservations.reduce((latest, reservation) => {
+    return new Date(reservation.check_out) > new Date(latest.check_out) ? reservation : latest;
+  }, guest.reservations[0]);
+  return formatDateTime(lastReservation.check_out);
+};
+
+export const calculateTotalAmountSpent = (reservations) => {
+  return reservations.reduce((total, reservation) => total + parseFloat(reservation.total_amount), 0).toFixed(2);
+};
