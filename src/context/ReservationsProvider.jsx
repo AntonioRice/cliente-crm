93349a28d4d758f56b3e1046c6e2359019629444
Antonio Rice <ricex407@gmail.com>
2024-06-10 18:08:22 -0500

    reservations provider

---
+++ src/context/ReservationsProvider.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, useContext, createContext } from "react";
+import axios from "axios";
+
+const ReservationsContext = createContext();
+
+export const useReservationsContext = () => useContext(ReservationsContext);
+
+export const ReservationsProvider = ({ children }) => {
+  const [reservationsAnalytics, setReservationsAnalytics] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchReservationsAnalytics = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("http://localhost:3015/api/v1/reservations/analytics");
+      console.log(response.data.data.reservationsByWeek);
+      setReservationsAnalytics(response.data.data.reservationsByWeek);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error("Error fetching reservations data", error);
+    }
+  };
+
+  return (
+    <ReservationsContext.Provider
+      value={{ fetchReservationsAnalytics, reservationsAnalytics, setReservationsAnalytics, loading, setLoading }}
+    >
+      {children}
+    </ReservationsContext.Provider>
+  );
+};
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,3 +1,4 @@
 export { AuthProvider } from "./AuthProvider";
 export { GuestProvider } from "./GuestProvider";
 export { StateProvider } from "./StateProvider";
+export { ReservationsProvider } from "./ReservationsProvider";
