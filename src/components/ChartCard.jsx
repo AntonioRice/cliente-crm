a5bd9344b57d5be3b6d77f5466e9c0ed322cbc0d
AntonioRice <ricex407@gmail.com>
2024-07-15 11:08:48 -0500

    chart card update

--- src/components/ChartCard.jsx
+++ src/components/ChartCard.jsx
@@ -1,22 +1,39 @@
 import { MdOutlineArrowBackIosNew, MdOutlineArrowForwardIos } from "react-icons/md";
+import { IoTrendingUpOutline, IoTrendingDownOutline, IoArrowForwardOutline } from "react-icons/io5";
 
-const ChartCard = ({ title, week, value, description, bgColor, handlePrevWeek, handleNextWeek }) => {
+const ChartCard = ({ title, week, value, description, bgColor, handlePrevWeek, handleNextWeek, delta }) => {
+  let TrendingIcon;
+  if (parseFloat(delta) > 0) {
+    TrendingIcon = IoTrendingUpOutline;
+  } else if (parseFloat(delta) < 0) {
+    TrendingIcon = IoTrendingDownOutline;
+  } else {
+    TrendingIcon = IoArrowForwardOutline;
+  }
+
   return (
     <div
-      className={`text-black rounded-xl border border-gray-200 dark:border-gray-700 p-6 opacity-80 ${bgColor} flex flex-col justify-between`}
+      className={`text-white rounded-xl border border-gray-200 dark:border-gray-700 p-6 opacity-80 ${bgColor} flex flex-col justify-between`}
     >
       <div>
         <h3 className="text-lg font-bold">{title}</h3>
         <p className="text-xs">Week: {week}</p>
-        <p className="mt-4 text-6xl font-bold">{value}</p>
-        <p className="mt-2 text-xs">{description}</p>
+      </div>
+      <div className="grid grid-cols-2 gap-2 mt-4">
+        <div className="flex flex-col items-start justify-center">
+          <p className="text-6xl font-bold">{value}</p>
+          <p className="mt-2 text-xs">{description}</p>
+        </div>
+        <div className="flex items-center justify-center">
+          <TrendingIcon size={150} />
+        </div>
       </div>
       <div className="flex justify-between mt-4">
-        <button onClick={handlePrevWeek} className="w-full hover:text-white">
-          <MdOutlineArrowBackIosNew className="float-start" />
+        <button onClick={handlePrevWeek} className="hover:text-white">
+          <MdOutlineArrowBackIosNew />
         </button>
-        <button onClick={handleNextWeek} className="w-full hover:text-white">
-          <MdOutlineArrowForwardIos className="float-right" />
+        <button onClick={handleNextWeek} className="hover:text-white">
+          <MdOutlineArrowForwardIos />
         </button>
       </div>
     </div>
