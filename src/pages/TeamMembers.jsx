3ec58bf8ca49dfc494ee2d603649f30343ea82b6
AntonioRice <ricex407@gmail.com>
2024-07-16 21:18:52 -0500

    team-members page

--- src/pages/TeamMembers.jsx
+++ src/pages/TeamMembers.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { DataTable } from "../components";
 
 const TeamMembers = () => {
-  return <div>TeamMembers</div>;
+  const [teamMembers, setTeamMembers] = useState([]);
+
+  useEffect(() => {
+    const fetchTeamMembers = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3015/api/v1/users`);
+        console.log(response.data.data);
+        setTeamMembers(response.data.data);
+      } catch (error) {
+        console.error("Error fetching team members data:", error);
+      }
+    };
+    fetchTeamMembers();
+  }, []);
+
+  if (!teamMembers.length) {
+    return <div>No Users Found</div>;
+  }
+
+  return <div>User</div>;
 };
 
 export default TeamMembers;
