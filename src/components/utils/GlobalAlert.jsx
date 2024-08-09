f8b6850b6e0d0ec2f913dccbc16f510dd14fcb32
AntonioRice <ricex407@gmail.com>
2024-08-09 18:17:44 -0500

    fade in effect alert

--- src/components/utils/GlobalAlert.jsx
+++ src/components/utils/GlobalAlert.jsx
@@ -1,5 +1,6 @@
 import Alert from "./Alert";
 import { useAlertContext } from "../../context";
+import { motion } from "framer-motion";
 
 const GlobalAlert = () => {
   const { alert } = useAlertContext();
@@ -7,9 +8,9 @@ const GlobalAlert = () => {
   if (!alert.visible) return null;
 
   return (
-    <div className="fixed bottom-0 left-1/2 z-50 w-full max-w-md -translate-x-1/2 transform">
+    <motion.div className="fixed bottom-0 left-1/2 z-50 w-full max-w-md -translate-x-1/2 transform" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 1 }}>
       <Alert message={alert.message} type={alert.type} />
-    </div>
+    </motion.div>
   );
 };
 
