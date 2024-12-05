72ff23f7732ee54b8e8089bd27b2993d7c5fc096
AntonioRice <ricex407@gmail.com>
2024-12-05 15:21:34 -0500

    rooms table

---
+++ src/components/features/dashboard/RoomsTable.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { IoIosArrowForward } from "react-icons/io";
+
+const RoomsChart = ({ rooms, handleRoomSelection }) => {
+  return (
+    <div className="flex flex-col overflow-hidden rounded-xl border border-gray-200 bg-gray-50 dark:border-gray-700 dark:bg-gray-800 md:col-span-2 xl:col-span-1">
+      <div className="p-5">
+        <h1 className="text-sm">Rooms</h1>
+        <p className="text-xs text-gray-500">Total: {rooms.length}</p>
+      </div>
+      <div className="scrollbar-hidden flex-1 overflow-auto">
+        <table className="w-full text-left text-xs text-gray-500 dark:text-gray-400">
+          <thead className="sticky top-0 bg-gray-50 uppercase dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th className="px-2 py-2 sm:px-4 md:px-6">Room</th>
+              <th className="px-2 py-2 sm:px-4 md:px-6">Name</th>
+              <th className="px-2 py-2 sm:px-4 md:px-6">Status</th>
+              <th className="px-2 py-2 sm:px-4 md:px-6">
+                <span className="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {rooms.map((room) => (
+              <tr key={room.number} className="border-t border-gray-500 first:border-t-0 last:border-b-0 hover:bg-gray-600 hover:text-white" onClick={() => handleRoomSelection(room.room_id)}>
+                <td className="px-2 py-4 sm:px-4 md:px-6">{room.number}</td>
+                <td className="px-2 py-4 sm:px-4 md:px-6">{room.name}</td>
+                <td className={`px-2 py-4 sm:px-4 md:px-6 ${room.occupied ? "text-red-500" : ""}`}>{room.occupied ? "Occupied" : "Available"}</td>
+                <td className="px-2 py-4 text-right sm:px-4 md:px-6">
+                  <button className="font-medium">
+                    <IoIosArrowForward />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default RoomsChart;
