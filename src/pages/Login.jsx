a26a7d7a41aeb0b7d8ac77f18e3682eda6fad132
AntonioRice <ricex407@gmail.com>
2024-12-09 09:56:09 -0500

    misc.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -79,7 +79,7 @@ const Login = () => {
                 <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
                   Welcome to <span className="ms-1 font-medium text-green-500">Cliente.io</span>
                 </h1>
-                <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">Your complete hotel guest management system</p>
+                <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">Effortless hotel guest management, all in one place.</p>
                 <a href="#" className="bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 mr-3 inline-flex items-center justify-center rounded-lg px-5 py-3 text-center text-base font-medium text-[#cccccc]">
                   Get started
                   <svg className="-mr-1 ml-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
