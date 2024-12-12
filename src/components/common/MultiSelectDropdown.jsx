935cfde7c9494a6ce98937519c10c62f1df63d6f
AntonioRice <ricex407@gmail.com>
2024-12-11 22:13:56 -0500

    room selection update

--- src/components/common/MultiSelectDropdown.jsx
+++ src/components/common/MultiSelectDropdown.jsx
@@ -52,20 +52,20 @@ const MultiSelectDropdown = ({ handleRoomsChange }) => {
       <button
         type="button"
         onClick={toggleDropdown}
-        className="hover:text-primary-700 flex h-[38px] w-full items-center justify-center rounded-lg  border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+        className="dark:focus:ring-neutral-700-700 flex h-[38px] w-full items-center justify-center rounded-lg border border-gray-300 bg-white p-4 px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-black focus:z-10 focus:outline-none focus:ring-1 focus:ring-gray-200 dark:border-neutral-600 dark:bg-neutral-700 dark:text-white  dark:hover:bg-neutral-600"
       >
         <CgMathPlus className="-ml-1 mr-1.5 size-4 text-green-400" />
         {selectedRoomsText}
       </button>
 
       {isOpen && (
-        <div className="absolute z-50 mt-4 w-full rounded-lg border border-gray-400 shadow dark:bg-gray-700">
+        <div className="absolute z-50 mt-4 w-full rounded-lg border shadow dark:border-neutral-600 dark:bg-neutral-700">
           <ul className="grid grid-cols-6 gap-2 p-4 text-sm">
             {rooms.map((room, i) => (
               <li key={i}>
                 <div
                   className={`flex items-center justify-center rounded p-2 ${reservationData.room_numbers?.includes(room.room_id) ? "bg-green-500 text-white" : ""} 
-                ${room.occupied ? "cursor-not-allowed text-red-500" : "dark:hover:bg-gray-600"}`}
+                ${room.occupied ? "cursor-not-allowed text-red-500" : "dark:hover:bg-neutral-600"}`}
                   onClick={() => !room.occupied && handleCheckboxChange(room)}
                 >
                   <label className="text-sm">{room.number}</label>
